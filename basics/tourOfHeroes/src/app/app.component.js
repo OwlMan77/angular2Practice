70141d274ce630979eaece9a403d46a7b8d47096
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-06-07 22:50:02 +0100

    added Hero class to app.compoent

--- basics/tourOfHeroes/src/app/app.component.js
+++ basics/tourOfHeroes/src/app/app.component.js
@@ -6,17 +6,24 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
+// Hero class
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
-        this.hero = 'Windstorm';
+        this.hero = { id: 1, name: 'Windstorm' };
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>{{title}}</h1><h2>{{hero}} details!</h2>",
+        template: "<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>",
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
