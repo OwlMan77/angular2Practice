94e0394024c8251069f34a600f3f0c41c157eea1
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-06-15 16:43:38 +0100

    added method navigate back and added it to hero-detail

--- basics/tourOfHeroes/src/app/hero-detail.component.js
+++ basics/tourOfHeroes/src/app/hero-detail.component.js
@@ -1,15 +1,5 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 var _this = this;
-var core_1 = require("@angular/core");
 var router_1 = require("@angular/router");
 var common_1 = require("@angular/common");
 var hero_service_1 = require("./hero.service");
@@ -20,20 +10,4 @@ ngOnInit();
 void {
     this: .route.params.switchMap(function (params) { return _this.heroService.getHero(+params['id']); }).subscribe(function (hero) { return _this.hero = hero; })
 };
-var HeroDetailComponent = (function () {
-    function HeroDetailComponent() {
-    }
-    return HeroDetailComponent;
-}());
-__decorate([
-    core_1.Input(),
-    __metadata("design:type", Object)
-], HeroDetailComponent.prototype, "hero", void 0);
-HeroDetailComponent = __decorate([
-    core_1.Component({
-        selector: 'hero-detail',
-        template: "<div *ngIf=\"hero\"> <h2>{{hero.name}} details!</h2> <div><label>id: </label>{{hero.id}}</div> <div> <label>name: </label> <input [(ngModel)]=\"hero.name\" placeholder=\"name\"/> </div> </div>",
-    })
-], HeroDetailComponent);
-exports.HeroDetailComponent = HeroDetailComponent;
 //# sourceMappingURL=hero-detail.component.js.map
