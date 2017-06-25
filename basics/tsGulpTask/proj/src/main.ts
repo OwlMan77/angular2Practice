85d3287471711513efe3841fd54dda26f3981019
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-06-25 17:41:38 +0100

    gulp, fixed typo and edited gulpfile to make task that runs browserfy, copies the html to dist and creates bundle.js

--- basics/tsGulpTask/proj/src/main.ts
+++ basics/tsGulpTask/proj/src/main.ts
@@ -2,7 +2,7 @@ import { sayHello } from "./greet";
 
 function showHello(divName: string, Name: string) {
   const elt = document.getElementById(divName);
-  elt.innerText = sayHello(name);
+  elt.innerText = sayHello(Name);
 }
 
 showHello("greeting", "TypeScript");
